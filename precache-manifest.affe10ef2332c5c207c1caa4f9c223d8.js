6684724b8aed0a92beef99b4ddeefa773dfc00dd
Anton Afanasiev <anton.afanasyev93@yandex.ru>
2020-09-26 01:05:36 +0300

    Updates

---
+++ precache-manifest.affe10ef2332c5c207c1caa4f9c223d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1de13f16ae9f3e127221d103d31e3e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f01f9aff4a7e4323343b",
+    "url": "./static/css/main.af4e0289.chunk.css"
+  },
+  {
+    "revision": "a77d879fd6dd19b833f1",
+    "url": "./static/js/2.252292ef.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "./static/js/2.252292ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01f9aff4a7e4323343b",
+    "url": "./static/js/main.a1b03241.chunk.js"
+  },
+  {
+    "revision": "643988e7aafbde4ec312",
+    "url": "./static/js/runtime-main.3b0feecc.js"
+  }
+]);
