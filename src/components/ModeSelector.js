73dec658efbaedc6f6ab9e4fc6873af4e5bf0659
Anton Afanasiev <anton.afanasyev93@yandex.ru>
2020-09-26 01:28:30 +0300

    hotfix 1: mixed content error

--- src/components/ModeSelector.js
+++ src/components/ModeSelector.js
@@ -13,8 +13,8 @@ export default props => {
   const classes = useStyles();
   return (
     <div className="ModeSelector">
-      <Button variant="contained" color="primary" onClick={() => props.onSelect('http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')}>32 items</Button>
-      <Button className={classes.button} variant="contained" color="secondary" onClick={() => props.onSelect('http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')}>1000 items</Button>
+      <Button variant="contained" color="primary" onClick={() => props.onSelect('https://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')}>32 items</Button>
+      <Button className={classes.button} variant="contained" color="secondary" onClick={() => props.onSelect('https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')}>1000 items</Button>
     </div>
   )
 }
